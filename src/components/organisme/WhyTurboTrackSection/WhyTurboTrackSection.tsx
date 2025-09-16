83225b6bd4a00944987868f8151e60b34bce1314
Aryan Salemabadi <aryanavatar1380@gmail.com>
2025-09-16 22:23:46 +0330

    add: add documents

--- src/components/organisme/WhyTurboTrackSection/WhyTurboTrackSection.tsx
+++ src/components/organisme/WhyTurboTrackSection/WhyTurboTrackSection.tsx
@@ -24,7 +24,7 @@ const WhyVeloraSection = () => {
       </motion.div>
 
       {/* Benefits Grid */}
-      <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8">
+      <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 sm:gap-8">
         {benefits.map(({ icon, title, description }, index) => (
           <motion.div
             key={title}
